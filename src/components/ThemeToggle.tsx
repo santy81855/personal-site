b7fe23c6401025ecd021a9999f4cb70560212b79
santy81855 <mynameissantiagogarcia@gmail.com>
2023-07-14 17:21:33 -0400

    created template for homepage and start of showcase page

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState, useEffect } from "react";
+import styles from "@/styles/ThemeToggle.module.css";
+
+export default function ThemeToggle() {
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+    useEffect(() => {
+        let curTheme = localStorage.getItem("theme");
+        if (curTheme === null) {
+            localStorage.setItem("theme", "light");
+        } else {
+            if (localStorage.getItem("theme") === "light") {
+                setIsDarkTheme(false);
+            } else {
+                setIsDarkTheme(true);
+            }
+        }
+    }, []);
+
+    const toggleTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+        document.documentElement.classList.toggle("dark-theme", !isDarkTheme);
+        // set the theme in local storage
+
+        if (localStorage.getItem("theme") === "light") {
+            console.log("setting to dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+            console.log("setting to light");
+        }
+    };
+
+    return (
+        <>
+            <button className={styles.themeToggle} onClick={toggleTheme}>
+                {isDarkTheme ? "Light" : "Dark"}
+            </button>
+        </>
+    );
+}
---
+++ src/pages/showcase/index.tsx
@@ -0,0 +1,175 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Showcase.module.css";
+import React, { useRef, useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Showcase() {
+    const [imageTitles, setImageTitles] = useState([
+        "Codon",
+        "Typo",
+        "Glass Cannon",
+        "AI Resume",
+    ]);
+
+    const [activeImage, setActiveImage] = useState(3);
+
+    const [imagesArray, setImagesArray] = useState([
+        <Image
+            className={`${styles.image1} ${styles.image}`}
+            src="/images/image1.png"
+            alt="Typo Image"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+        />,
+        <Image
+            className={`${styles.image2} ${styles.image}`}
+            src="/images/image2.png"
+            alt="Codon Image"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+        />,
+        <Image
+            className={`${styles.image3} ${styles.image}`}
+            src="/images/image3.png"
+            alt="Glass Cannon Image"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+        />,
+        <Image
+            className={`${styles.activeImage} ${styles.image}`}
+            src="/images/image4.png"
+            alt="AI Resume Image"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+        />,
+    ]);
+
+    const changeActiveImage = () => {
+        setImagesArray((prevArray) => {
+            const newArray = prevArray.map((image, i) => {
+                if (i === 3) {
+                    // Modify the style of the desired element
+                    return (
+                        <Image
+                            className={`${styles.activeImage} ${styles.image}`}
+                            src={image.props.src}
+                            alt={image.props.alt}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    );
+                }
+                return image; // Return other elements as they are
+            });
+            return newArray;
+        });
+    };
+
+    const cycleLeft = () => {
+        // reorder the imagesArray useState array by moving everything to the left
+        // and moving the first element to the end
+        // then set the state to the new array
+        setImagesArray((prevArray) => {
+            const newArray = [...prevArray];
+            const firstElement = newArray.shift();
+            newArray.push(firstElement);
+            return newArray;
+        });
+        changeActiveImage();
+        // change the activeImage state to the new active image index
+        setActiveImage((prevIndex) => {
+            if (prevIndex === 3) {
+                return 0;
+            } else {
+                return prevIndex + 1;
+            }
+        });
+    };
+    const cycleRight = () => {
+        setImagesArray((prevArray) => {
+            const newArray = [...prevArray];
+            const lastElement = newArray.pop();
+            newArray.unshift(lastElement);
+            return newArray;
+        });
+        changeActiveImage();
+        setActiveImage((prevIndex) => {
+            if (prevIndex === 0) {
+                return 3;
+            } else {
+                return prevIndex - 1;
+            }
+        });
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Project showcase.</title>
+                <meta name="description" content="List of my projects." />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={`${styles.main} ${inter.className}`}>
+                <section className={styles.section}>
+                    <div className={`${styles.leftItem} ${styles.titleItem}`}>
+                        <p>example small title</p>
+                        <h1>
+                            Example big title for you that is amazing and cool
+                        </h1>
+                    </div>
+                    <div className={`${styles.rightItem} ${styles.titleItem}`}>
+                        <p>
+                            Example description of my work and technologies i am
+                            familiar with
+                        </p>
+                    </div>
+                </section>
+                <div className={styles.imageSectionContainer}>
+                    <div className={styles.imagesContainer}>
+                        <div className={styles.arrowContainer}>
+                            <div className={styles.imageTitle}>
+                                {imageTitles[activeImage]}
+                            </div>
+                            <div
+                                className={styles.leftArrow}
+                                onClick={() => cycleLeft()}
+                            ></div>
+                            <div className={styles.circle}></div>
+                            <div
+                                className={styles.rightArrow}
+                                onClick={() => cycleRight()}
+                            ></div>
+                        </div>
+                        <div className={`${styles.image1} ${styles.image}`}>
+                            {imagesArray[0]}
+                        </div>
+                        <div className={`${styles.image2} ${styles.image}`}>
+                            {imagesArray[1]}
+                        </div>
+                        <div className={`${styles.image3} ${styles.image}`}>
+                            {imagesArray[2]}
+                        </div>
+                        <div
+                            className={`${styles.activeImage} ${styles.image}`}
+                        >
+                            {imagesArray[3]}
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.display1}>hi</div>
+            </main>
+        </>
+    );
+}
