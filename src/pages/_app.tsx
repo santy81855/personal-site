e3b240e07e1c8114ff6a6d5ae469150c1b07a366
santy81855 <mynameissantiagogarcia@gmail.com>
2023-07-21 08:04:14 -0400

    fixed nav bar not sticking past a certain scroll point

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { useEffect, useState } from "react";
+import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
     // set the correct theme based on the user's preference
